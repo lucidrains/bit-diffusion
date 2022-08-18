5d86f55439bee9a252e102e0deee2669030f3813
Phil Wang <lucidrains@gmail.com>
2022-08-17 17:56:04 -0700

    cleanup

--- bit_diffusion/bit_diffusion.py
+++ bit_diffusion/bit_diffusion.py
@@ -1,9 +1,7 @@
 import math
-import copy
 from pathlib import Path
 from random import random
 from functools import partial
-from collections import namedtuple
 from multiprocessing import cpu_count
 
 import torch
