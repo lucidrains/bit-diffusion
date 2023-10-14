e2c94c655959f80d3ca2a13f75114ac14694eda6
Phil Wang <lucidrains@gmail.com>
2023-10-14 07:43:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'bit-diffusion',
   packages = find_packages(exclude=[]),
-  version = '0.1.2',
+  version = '0.1.4',
   license='MIT',
   description = 'Bit Diffusion - Pytorch',
   author = 'Phil Wang',
